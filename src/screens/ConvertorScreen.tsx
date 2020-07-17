430fc222555d91239ee5494b1d77b61e001af267
Jamie Ingram <jr-ingram@hotmail.co.uk>
2020-07-17 08:57:15 +0100

    Allow ability to add multiple inputs

--- src/screens/ConvertorScreen.tsx
+++ src/screens/ConvertorScreen.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, Button } from 'react-native';
 import Title from '../components/title';
 import UnitConvertorRow from '../components/UnitConvertorRow';
 
+const getRows = (totalInputRows: number) => {
+	const inputRows = [];
+	for (let i = 0; i < totalInputRows; i++) {
+		inputRows.push(<UnitConvertorRow />);
+	}
+	return inputRows;
+};
+
 const ConvertorScreen = (): JSX.Element => {
+	const [numberOfInputs, setNumberOfInputs] = useState(1);
+	const rows = getRows(numberOfInputs);
+
 	return (
 		<ScrollView style={styles.container}>
 			<Title title="Unit Convertor" />
-			<UnitConvertorRow />
+			{rows}
+			<Button
+				onPress={() => setNumberOfInputs(numberOfInputs + 1)}
+				title="Add Input (+)"
+				color="#009900"
+			/>
+			<Button
+				onPress={() => {
+					// TODO - DON'T ALLOW THE REMOVAL OF FINAL INPUT
+					setNumberOfInputs(numberOfInputs - 1);
+				}}
+				title="Remove Input (-)"
+				color="#990000"
+			/>
 		</ScrollView>
 	);
 };
